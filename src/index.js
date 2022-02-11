94e127bb41c73864a473145176b7de0c4ac02789
victor pena <victor.pena1392@Gmail.com>
2022-02-11 14:08:26 -0600

    able to add file to json files and load them on page without refreshing

--- src/index.js
+++ src/index.js
@@ -10,22 +10,24 @@ sayHello('Victor Pena');
 const {getMovies, addMovie} = require('./api.js');
 const $ = require('jquery');
 
-getMovies().then((movies) => {
-  let htmlMovies = '';
-  movies.forEach(({title, rating, id}) => {
-    htmlMovies += `<div><input type="checkbox" value="${id}">${title} Rating: ${rating}</input></div>`
+function loadMovies() {
+  getMovies().then((movies) => {
+    let htmlMovies = '';
+    movies.forEach(({title, rating, id}) => {
+      htmlMovies += `<div><input type="checkbox" value="${id}">${title} Rating: ${rating}</input></div>`
+    });
+    $('#movie-list').html(htmlMovies);
+  }).catch((error) => {
+    alert('Oh no! Something went wrong.\nCheck the console for details.')
+    console.log(error);
   });
-  $('#movie-list').html(htmlMovies);
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
-});
-
+}
+loadMovies()
 $('#submit-new-movie').click(function () {
   let movieTitle = $('#new-movie').val();
   let rating = $('#rating').val();
   addMovie(movieTitle, rating).then((response) =>{
-
+    loadMovies();
   })
 });
 
