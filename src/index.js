9b2e14519907363fa985a70687d1aa7be531ece1
victor pena <victor.pena1392@Gmail.com>
2022-02-11 12:23:17 -0600

    pulled movie through restful API and displayed them in HTML

--- src/index.js
+++ src/index.js
@@ -8,13 +8,18 @@ sayHello('Victor Pena');
  * require style imports
  */
 const {getMovies} = require('./api.js');
+const $ = require('jquery');
 
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
+  let htmlMovies = '';
   movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+    htmlMovies += `<div>${title}</div>`
+
+    // let movie =`id#${id} - ${title} - rating: ${rating}`;
   });
+  $('#movie-list').html(htmlMovies);
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
